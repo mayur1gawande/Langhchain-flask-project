1d196973856395c427397dac25b8460e9178b800
Mayur Suresh Gawande <141245128+mayur1gawande@users.noreply.github.com>
2025-08-08 13:18:33 +0530

    Update app.py
    added the os file and also set the port and host including changed debug=False

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from model import run_agent
+import os
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -20,4 +21,6 @@ def chat():
     return jsonify({"response": str(bot_response)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False,host='0.0.0.0',port=port)
+
